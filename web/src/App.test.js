2bec480c7e970fa5ad7c3ef3e525bd40dddc67c6
Mohammad Uddin <s184174@student.dtu.dk>
2022-10-25 14:19:07 +0200

    slet

--- web/src/App.test.js
+++ web/src/App.test.js
@@ -6,6 +6,4 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 
-  let usernameField = screen.getByLabelText("username");
-  expect(usernameField).toHaveAttribute("placeholder","indtast brugernavn")
 });
